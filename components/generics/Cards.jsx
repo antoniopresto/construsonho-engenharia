a73d42496313cfcc7a2e20cbf0387c7519175476
rafa meira <meirarafaela@hotmail.com>
2022-03-10 16:27:41 -0300

    responsividade cards

--- components/generics/Cards.jsx
+++ components/generics/Cards.jsx
@@ -1,28 +1,26 @@
 import React from "react";
-import { Col } from "antd";
+import {Col} from "antd";
 
 export default function Cards(props) {
-  const { subtitulo, linkimg, description } = props;
-  return (
-    <>
-      <Col flex="1 1 200px" xl={{ offset: 2 }}>
-        <div className="flip-container">
-          <div className="flipper">
-            <div
-              className="front"
-              style={{ background: linkimg, backgroundSize: "cover" }}
-            ></div>
-            <div className="back">
-              <div className="underlaine__card" />
-              <p className="outhertexts">{description}</p>
-            </div>
-          </div>
-        </div>
-        <div style={{ paddingBottom: "1rem" }}>
-          <p className="subtitlesection">{subtitulo}</p>
-          <div className="underlaine__card" />
-        </div>
-      </Col>
-    </>
-  );
+    const {subtitulo, linkimg, description} = props;
+    return (
+        <>
+            <Col flex="1 1 250px" >
+                <div className="container_cards" style={{background: linkimg, backgroundSize: "cover"}}>
+                    <div className="overlay outhertexts">
+                        <div className="items head">
+                            <p>{description}</p>
+                            <hr/>
+                        </div>
+                      <div className="items"/>
+                    </div>
+                </div>
+
+                <div style={{paddingBottom: "1rem"}}>
+                    <p className="subtitlesection">{subtitulo}</p>
+                    <div className="underlaine__card"/>
+                </div>
+            </Col>
+        </>
+    );
 }
